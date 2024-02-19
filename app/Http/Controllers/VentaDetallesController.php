eded6d18759ac2416ae54221b637eed23ac4a9b3
marti_rodriguez <marti.rodriguezp@mataro.epiaedu.cat>
2024-02-19 18:51:46 +0100

    creados recursos y añadido breeze

---
+++ app/Http/Controllers/VentaDetallesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VentaDetalles;
+use Illuminate\Http\Request;
+
+class VentaDetallesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VentaDetalles $ventaDetalles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VentaDetalles $ventaDetalles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, VentaDetalles $ventaDetalles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VentaDetalles $ventaDetalles)
+    {
+        //
+    }
+}
